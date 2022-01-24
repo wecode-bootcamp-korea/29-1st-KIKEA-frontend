344adbee072f41bd59a838361856f7d96de5c500
moonkorea00 <moonkorea00@gmail.com>
2022-01-24 19:32:07 +0900

    test

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './Nav.scss';
 
 function Nav() {
-  return <div>
-    slack 연동
-  </div>;
+  return (
+    <>
+      <div className="main">
+        <section className="menu">
+          <button className="menu-toggle-btn" onClick="">
+            toggle
+          </button>
+        </section>
+        <div className="main-nav">
+          <div className="nav-left">
+            <img className="kikea-logo" alt="kikea" src="" />
+          </div>
+          <div className="nav-center"></div>
+          <div className="nav-right"></div>
+        </div>
+        <div className="products-menu">
+          <ul>
+            <li>모든 제품</li>
+            <li>온라인 쇼룸</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Nav;
