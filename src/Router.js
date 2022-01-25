b468b422abd55d09dc9ecfc23b212fe2ed5be654
cozynye <cozynye@gmail.com>
2022-01-25 19:00:50 +0900

    add : main page add

--- src/Router.js
+++ src/Router.js
@@ -8,7 +8,7 @@ function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route exact path="/detail" element={<Detail />} />
+        <Route path="/detail" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
