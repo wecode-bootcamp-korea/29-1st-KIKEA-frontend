e055268d95f0f35e2c10658667a9e251f8000046
cozynye <cozynye@gmail.com>
2022-01-25 18:55:45 +0900

    add : main page add

--- src/pages/Main/MainProduct/MainProduct.js
+++ src/pages/Main/MainProduct/MainProduct.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './MainProduct.scss';
 import ProductFilter from './ProductFilter/ProductFilter';
 import ProductItem from './ProductItem/ProductItem';
 
 const MainProduct = () => {
   return (
-    <div className="mainProduct-wrapper">
+    <div className="mainproduct-wrapper">
       <h2 className="main-wrapper-homefurnishing">홈 퍼니싱 상품</h2>
       <ProductFilter />
       <ProductItem />
