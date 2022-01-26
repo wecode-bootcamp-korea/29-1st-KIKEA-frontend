c8f1560ab65b402b04b4fe85e510c94156747b4d
cozynye <cozynye@gmail.com>
2022-01-26 16:28:26 +0900

    add : main page ui

--- src/pages/Main/Promotion/PromotionCard/PromotionCard.js
+++ src/pages/Main/Promotion/PromotionCard/PromotionCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './PromotionCard.scss';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const PromotionCard = ({ title, img, description }) => {
   return (
@@ -10,7 +12,9 @@ const PromotionCard = ({ title, img, description }) => {
         <p>{description}</p>
         <div className="arrow-wrapper">
           <span className="arrow-span">
-            <button className="arrwo-button">-></button>
+            <button className="arrwo-button">
+              <FontAwesomeIcon icon={faArrowRight} size="2x" />
+            </button>
           </span>
         </div>
       </div>
