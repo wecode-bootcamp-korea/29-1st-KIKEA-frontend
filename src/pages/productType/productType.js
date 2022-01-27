8cdcdd153e5e6156d3b48a4f86a60e10e7a3720f
moonkorea00 <moonkorea00@gmail.com>
2022-01-27 13:07:36 +0900

    수정: productType

--- src/pages/productType/productType.js
+++ src/pages/productType/productType.js
@@ -1,6 +1,21 @@
 import React from 'react-dom';
 
 const ProductType = () => {
-  return <>gi</>;
+  return (
+    <>
+      <div className="product-type-page-container">
+        <div clasName="category-path">
+          <ul className="category-path-list">
+            <li className="category-path-items">제품</li>
+            <li className="category-path-items">></li>
+            <li className="category-path-items">가구</li>
+            <li className="category-path-items">></li>
+            <li className="category-path-items">소파</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
-export default ProducType;
+
+export default ProductType;
