c9cdf7550b6781273894a8823274dee65ff07266
moonkorea00 <moonkorea00@gmail.com>
2022-02-03 18:16:18 +0900

    수정: mock data로 수정

--- src/pages/ProductType/ProductCategory/ProductCategory.js
+++ src/pages/ProductType/ProductCategory/ProductCategory.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import './ProductCategory.scss';
 
-const ProductCategory = ({ productData }) => {
-  return (
-    <div className="product-category-container">
-      <div clasName="category-path">
-        <p className="category-path-items">제품 > 가구 > 소파</p>
+const ProductCategory = () =>
+  // { productData }
+  {
+    return (
+      <div className="product-category-container">
+        <div clasName="category-path">
+          <p className="category-path-items">제품 > 가구 > 소파</p>
+        </div>
+        <h1 className="category-title">소파</h1>
+        <div className="product-category-wrapper">
+          {
+            // productData.categories &&
+            //   productData.categories
+            CATEGORYDATA.map(kikea => (
+              <div className="product-category" key={kikea.id}>
+                <img
+                  className="product-category-img"
+                  src={kikea.img}
+                  alt={kikea.description}
+                />
+                <p className="product-category-name">{kikea.categoryName}</p>
+              </div>
+            ))
+          }
+        </div>
       </div>
-      <h1 className="category-title">소파</h1>
-      <div className="product-category-wrapper">
-        {productData.categories &&
-          productData.categories.map(kikea => (
-            <div className="product-category" key={kikea.id}>
-              <img
-                className="product-category-img"
-                src={kikea.img}
-                alt={kikea.description}
-              />
-              <p className="product-category-name">{kikea.categoryName}</p>
-            </div>
-          ))}
-      </div>
-    </div>
-  );
-};
+    );
+  };
 
 const CATEGORYDATA = [
   {
--- src/pages/ProductType/ProductFilter/ProductFilter.js
+++ src/pages/ProductType/ProductFilter/ProductFilter.js
@@ -3,51 +3,50 @@ import './ProductFilter.scss';
 
 import ProductCard from './ProductCard/ProductCard';
 
-const ProductFilter = ({ productData }) => {
-  return (
-    <div className="filter-section-container">
-      <div className="filter-btn-container">
-        <button type="button" className="filter-btn">
-          비교
-        </button>
-        <button type="button" className="filter-btn">
-          정렬
-        </button>
-        <button type="button" className="filter-btn">
-          시트수
-        </button>
-        <button type="button" className="filter-btn">
-          카테고리
-        </button>
-        <button type="button" className="filter-btn">
-          가격
-        </button>
-        <button type="button" className="filter-btn">
-          사이즈
-        </button>
-        <button type="button" className="filter-btn">
-          색상
-        </button>
-        <button type="button" className="filter-btn">
-          소재
-        </button>
+const ProductFilter = () =>
+  // { productData }
+  {
+    return (
+      <div className="filter-section-container">
+        <div className="filter-btn-container">
+          <button type="button" className="filter-btn">
+            비교
+          </button>
+          <button type="button" className="filter-btn">
+            정렬
+          </button>
+          <button type="button" className="filter-btn">
+            시트수
+          </button>
+          <button type="button" className="filter-btn">
+            카테고리
+          </button>
+          <button type="button" className="filter-btn">
+            가격
+          </button>
+          <button type="button" className="filter-btn">
+            사이즈
+          </button>
+          <button type="button" className="filter-btn">
+            색상
+          </button>
+          <button type="button" className="filter-btn">
+            소재
+          </button>
+        </div>
+        <div className="filter-btn-category" />
+        <div className="product-card-wrapper">
+          {
+            // productData.categories &&
+            //   productData.categories
+            PRODUCTDATA.map(kikea => {
+              return <ProductCard key={kikea.id} kikea={kikea} />;
+            })
+          }
+        </div>
       </div>
-      <div className="filter-btn-category" />
-      <div className="product-card-wrapper">
-        {productData.categories &&
-          productData.categories.map(kikea => {
-            return (
-              <ProductCard
-                // productData={productData}
-                key={kikea.id}
-                kikea={kikea}
-              />
-            );
-          })}
-      </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default ProductFilter;
 
