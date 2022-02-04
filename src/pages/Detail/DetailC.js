6aa73299b2543e96cf0e7273a89f5ff56cddbaa5
박혜린 <likehyelin@gmail.com>
2022-02-04 23:38:16 +0900

    구매하기, 제품 설명 sidebar toggle 기능 구현 - 외부 클릭 시 sidebar close 추가, 리팩토링

--- src/pages/Detail/DetailC.js
+++ src/pages/Detail/DetailC.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import DetailNav from './DetailNav';
 import DetailMain from './DetailMain/DetailMain';
 import DetailSide from './DetailSide/DetailSide';
@@ -8,19 +8,20 @@ import './DetailC.scss';
 
 const Detail = () => {
   const [productBox, setProductBox] = useState([]);
-  const [isOpen, setIsOpen] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
   const [descSideOpen, setDescSideOpen] = useState(false);
 
-  const toggleAddBtn = () => {
+  const toggleCartBtn = () => {
     setIsOpen(!isOpen);
-  };
-
-  const toggleCloseBtn = () => {
-    setIsOpen(false);
   };
 
   const toggleDescBtn = () => {
     setDescSideOpen(!descSideOpen);
+  };
+
+  const toggleSideBar = () => {
+    if (isOpen) return toggleCartBtn();
+    if (descSideOpen) return toggleDescBtn();
   };
 
   useEffect(() => {
@@ -31,57 +32,23 @@ const Detail = () => {
       });
   }, []);
 
-  // const TOTAL_SLIDES = 4;
-  // const [currentSlide, setCurrentSlide] = useState(0);
-  // const slideRef = useRef(null);
-
-  // const NextSlide = () => {
-  //   if (currentSlide >= TOTAL_SLIDES) {
-  //     setCurrentSlide(0);
-  //   } else {
-  //     setCurrentSlide(currentSlide + 1);
-  //   }
-  // };
-
-  // const PrevSlide = () => {
-  //   if (currentSlide === 0) {
-  //     setCurrentSlide(TOTAL_SLIDES);
-  //   } else {
-  //     setCurrentSlide(currentSlide - 1);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   slideRef.current.style.transition = 'all 0.5s ease-in-out';
-  //   slideRef.current.style.transform = `translateX(-${currentSlide}00%)`;
-  // }, [currentSlide]);
-
-  // const out = useRef();
-
-  // const handleClose = e => {
-  //   if (isOpen && !out.current.contains(e.target)) setIsOpen(null);
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener('click', handleClose);
-  //   return () => {
-  //     window.removeEventListener('click', handleClose);
-  //   };
-  // }, []);
-
   return (
     <main className="detail-page-container">
       <div className="detail-page">
         <DetailNav />
         <div className="detail-container">
+          <div
+            className={isOpen || descSideOpen ? 'empty-ele' : 'empty-ele close'}
+            onClick={toggleSideBar}
+          />
           <DetailMain productBox={productBox} toggleDescBtn={toggleDescBtn} />
           <DetailSide
             isOpen={isOpen}
-            toggleAddBtn={toggleAddBtn}
+            toggleAddBtn={toggleCartBtn}
             productBox={productBox}
           />
           {isOpen && (
-            <DetailAside isOpen={isOpen} toggleCloseBtn={toggleCloseBtn} />
+            <DetailAside isOpen={isOpen} toggleCloseBtn={toggleCartBtn} />
           )}
           <DetailDescSide descSideOpen={descSideOpen} />
         </div>
