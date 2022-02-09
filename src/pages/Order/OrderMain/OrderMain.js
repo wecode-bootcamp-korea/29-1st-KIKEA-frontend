73fda20b48180a741a6c8d68794741940fadb8b2
cozynye <cozynye@gmail.com>
2022-02-09 10:12:34 +0900

    add:order

--- src/pages/Order/OrderMain/OrderMain.js
+++ src/pages/Order/OrderMain/OrderMain.js
@@ -9,8 +9,81 @@ const OrderMain = () => {
         </div>
         <div className="order-text">
           <p>배송 방법</p>
-          <p>우편번호를 입력하세요</p>
-          <input type="text" />
+          <p>주소를 입력하세요</p>
+          <div className="information">
+            <div className="input-box">
+              <input
+                className="member-email"
+                type="text"
+                name="email"
+                onChange={changeInfo}
+                placeholder="  "
+                required
+                autoComplete="off"
+              />
+              <label className="form-label">
+                <span>이메일</span>
+              </label>
+            </div>
+            <div className="input-box">
+              <input
+                className="member-password"
+                type="password"
+                name="password"
+                placeholder="  "
+                onChange={changeInfo}
+                required
+              />
+              <label className="form-label">
+                <span>비밀번호</span>
+              </label>
+            </div>
+            <div className="input-box">
+              <input
+                className="member-name"
+                type="text"
+                name="name"
+                placeholder="  "
+                onChange={changeInfo}
+                required
+                autoComplete="off"
+              />
+              <label className="form-label">
+                <span>이름</span>
+              </label>
+            </div>
+            <div className="input-box">
+              <input
+                className="member-phoneNumber"
+                type="text"
+                name="phoneNumber"
+                placeholder="  "
+                onChange={changeInfo}
+                required
+                autoComplete="off"
+              />
+              <label className="form-label">
+                <span>휴대전화</span>
+              </label>
+            </div>
+            <div className="input-box">
+              <input
+                className="member-address"
+                type="text"
+                name="address"
+                placeholder="  "
+                onChange={changeInfo}
+                required
+                autoComplete="off"
+              />
+              <label className="form-label">
+                <span>주소</span>
+              </label>
+            </div>
+            <button className="signupButton" onClick={signUp}>
+              KIKEA의 가족이 되기
+            </button>
+          </div>
         </div>
       </div>
     </div>
