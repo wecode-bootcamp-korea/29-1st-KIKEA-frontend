b92f202c5ec1a5935905a5796eecaf27c61c9cac
moonkorea00 <moonkorea00@gmail.com>
2022-02-09 11:52:02 +0900

    수정: merge

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import Main from './pages/Main/Main';
 import ProductTypeMain from './pages/ProductType/ProductTypeMain';
 import Member from './pages/Member/Member';
 import DetailC from './pages/Detail/DetailC';
+import Nav from './components/Nav';
 
 function Router() {
   return (
