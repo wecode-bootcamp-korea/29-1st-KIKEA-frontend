b3bc2b9c9ca46dae9484236d9edaf20139649d28
cozynye <cozynye@gmail.com>
2022-02-09 11:07:24 +0900

    order page

--- src/pages/Order/OrderInfo/OrderInfo.js
+++ src/pages/Order/OrderInfo/OrderInfo.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './OrderInfo.scss';
 
 const OrderInfo = () => {
-  return <div className="orderinfo">order info</div>;
+  return (
+    <div className="orderinfo">
+      <div className="order-data">
+        
+      </div>
+    </div>
+  );
 };
 
 export default OrderInfo;
